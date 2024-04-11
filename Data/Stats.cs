a10ed935b7ae8a53ef9f43780cab10a862785162
Ross Cowie <rossfcowie@gmail.com>
2024-04-11 19:41:04 +0100

    Created all cards for collections.  By stacking divs with background images I've imposed the cardback, base, stars and numbers. I also simplified the sotrage of the collection, now it just stores 2 bytes per card, one for the stars and one for the rarity.  Next step is to add unlocking them to the game.

--- Data/Stats.cs
+++ Data/Stats.cs
@@ -1,29 +1,197 @@
 ﻿namespace CoOpCalculator.Data
 {
      
-    public class Stats
+    public class Stats()
     {
-        public int SuccessCount { get; set; } = 0;
-        public int fastest { get; set; } = 0;
-        public List<string> collection = new List<string>();
-        public Stats()
+        public byte[] collection { get; set; } = new byte[90];
+        public String getCardBacks(byte i)
         {
-            SuccessCount = 0;
-            fastest = 0;
-            for(int i = 10; i < 100; i++)
+            switch (i)
             {
-                collection.Add("");
+                case <= 0:
+                    return "";
+                case > 0 and <= 7:
+                    return "B";
+                case > 7 and <= 14:
+                    return "S";
+                case > 14 and <= 21:
+                    return "BS";
+                case > 21 and <= 28:
+                    return "BG";
+                case > 28 and <= 35:
+                    return "G";
+                case > 35 and <= 42:
+                    return "GS";
+                case > 42 and <= 49:
+                    return "BGS";
+                default:
+                    return "";
+
             }
         }
-        public Boolean markdone(int i, int gm)
+
+        public byte getCardBacks(string i)
         {
-            if (collection[i].Contains(gm.ToString()))
+            switch (i)
+            {
+                case "B":
+                    return 0;
+                case "S":
+                    return 7;
+                case "BS":
+                    return 14;
+                case "BG":
+                    return 21;
+                case "G":
+                    return 28;
+                case "GS":
+                    return 35;
+                case "BGS":
+                    return 42;
+                default:
+                    return 0;
+            }
+        }
+        public string getCardStars(byte i)
+        {
+            switch (i%7)
+            {
+                case 1:
+                    return "R";
+                case 2:
+                    return "L";
+                case 3:
+                    return "M";
+                case 4:
+                    return "RL";
+                case 5:
+                    return "RM";
+                case 6:
+                    return "ML";
+                case 0:
+                    return "RML";
+                default:
+                    return "";
+            }
+            return "";
+        }
+        public byte getCardStars(string i)
+        {
+            switch (i)
+            {
+                case "R":
+                    return 1;
+                case "L":
+                    return 2;
+                case "M":
+                    return 3;
+                case "RL":
+                    return 4;
+                case "RM":
+                    return 5;
+                case "ML":
+                    return 6;
+                case "RML":
+                    return 7;
+                default:
+                    return 0;
+
+            }
+            return 0;
+        }
+
+        public Boolean getCard(int i, string level)
+        {
+            byte b = (byte)collection.GetValue(i - 10);
+            string c = getCardBacks(b);
+            string s = getCardStars(b);
+            if (c.Contains(level))
             {
                 return false;
             }
             else
             {
-                collection[i] += (gm.ToString());
+                if (level.Equals("B"))
+                {
+                    c = "B" + c;
+                }
+                if (level.Equals("G"))
+                {
+                    if (c.Contains("B"))
+                    {
+                        c = c + "G";
+                    }
+                    else
+                    {
+
+                        if (c.Contains("S"))
+                        {
+                            c = "G" + c;
+                        }
+                        else
+                        {
+                            c = "G" + c;
+                        }
+
+                    }
+                }
+                if (level.Equals("S"))
+                {
+                    c = c + "S";
+                }
+                if (c.Length >= 3)
+                {
+                    c = "BGS";
+                }
+                collection.SetValue((byte)(getCardBacks(c) + getCardStars(s)), i-10);
+                return true;
+            }
+        }
+
+        public Boolean getStar(int i, string gm)
+        {
+            byte b = (byte)collection.GetValue(i-10);
+            string s = getCardBacks(b);
+            string c = getCardStars(b);
+            if (c.Contains(gm))
+            {
+                return false;
+            }
+            else
+            {
+                if (gm.Equals("R"))
+                {
+                    c = "R" + c;
+                }
+                if (gm.Equals("M"))
+                {
+                    if ( c.Contains("R"))
+                {
+                    c = c + "M";
+                }
+                else
+                {
+
+                    if (c.Contains("L"))
+                    {
+                        c = "M" + c;
+                    }
+                    else
+                    {
+                            c = "M" + c;
+                        }
+
+                    }
+                }
+                if (gm.Equals("L"))
+                {
+                    c = c + "L";
+                }
+                if (c.Length >= 3)
+                {
+                    c = "RML";
+                }
+                collection.SetValue((byte)(getCardBacks(s) + getCardStars(c)), i - 10);
                 return true;
             }
         }
